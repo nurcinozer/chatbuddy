f37e6f08cb7473bbd5cc7a85173e6c73f0453216
nurçin özer <nurcinpiksel@outlook.com>
2020-04-30 01:52:24 +0300

    socket.io - redis installation

--- src/socketApi.js
+++ src/socketApi.js
@@ -4,6 +4,13 @@ const io = socketio();
 const socketApi = {};
 socketApi.io = io;
 
+//redis adapter
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection', socket => {
     console.log('a user logged in');
 });
